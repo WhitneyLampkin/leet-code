7525cf7d4d621378011ec1bb30ad0e4297b41373
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-07-26 10:01:32 -0500

    Update 2652-sum-multiples.go - adding solution

--- 2652-sum-multiples.go
+++ 2652-sum-multiples.go
@@ -1,1 +1,17 @@
+func sumOfMultiples(n int) int {
+    // Declare variables
+    total := 0
 
+    // Iterate through each number in the range
+        // Check if divisible by 3, 5, or 7
+            // Yes? - Add to the overall total
+            // No? - Go to next value
+    for i := 1; i <= n; i++ {
+        if (i % 3) == 0 || (i % 5) == 0 || (i % 7) == 0 {
+            total += i
+        }
+    }
+
+    // return sum of valid numbers
+    return total
+}
