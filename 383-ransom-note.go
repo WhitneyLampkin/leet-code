b4eca45354892ec5f8d920221262c8eae666c9ed
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-08-04 09:52:06 -0500

    Update 383-ransom-note.go

--- 383-ransom-note.go
+++ 383-ransom-note.go
@@ -24,3 +24,13 @@ Constraints:
 1 <= ransomNote.length, magazine.length <= 105
 ransomNote and magazine consist of lowercase English letters.
 */
+
+func canConstruct(ransomNote string, magazine string) bool {
+    for _, v := range ransomNote {
+		if strings.Count(ransomNote, string(v)) > strings.Count(magazine, string(v)) {
+			return false
+		}
+	}
+    
+	return true
+}
