c330394838c9b040d1a76965b57893bb2188b3f4
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-08-10 13:18:20 -0500

    Update 20-valid-parentheses.go

--- 20-valid-parentheses.go
+++ 20-valid-parentheses.go
@@ -32,3 +32,61 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 */
+
+// Original solution
+/*
+hasOpenParenthesis := false
+    hasOpenBracket := false
+    hasOpenCurlyBrace := false
+
+    // Iterate through s string
+    for _, value := range s {
+        // Open bracket?
+            // Check if appropriate flag is true
+                // Yes - return false
+                // No - Set appropriate flag to true
+        // Closing bracket?
+            // Check if appropriate flag is true
+                // Yes - Set flag to false and continue
+                // No - return false
+        switch value {
+            case '(':
+                if hasOpenParenthesis || hasOpenBracket || hasOpenCurlyBrace {
+                    return false
+                } else {
+                    hasOpenParenthesis = true
+                }
+            case ')':
+                if hasOpenParenthesis && !hasOpenBracket && !hasOpenCurlyBrace {
+                    hasOpenParenthesis = false
+                } else {
+                    return false
+                }
+            case '[':
+                if hasOpenParenthesis || hasOpenBracket || hasOpenCurlyBrace {
+                    return false
+                } else {
+                    hasOpenBracket = true
+                }
+            case ']':
+                if !hasOpenParenthesis && hasOpenBracket && !hasOpenCurlyBrace {
+                    hasOpenBracket = false
+                } else {
+                    return false
+                }
+            case '{':
+                if hasOpenParenthesis || hasOpenBracket || hasOpenCurlyBrace {
+                    return false
+                } else {
+                    hasOpenCurlyBrace = true
+                }
+            case '}':
+                if !hasOpenParenthesis && !hasOpenBracket && hasOpenCurlyBrace {
+                    hasOpenCurlyBrace = false
+                } else {
+                    return false
+                }
+            default:
+                return false
+        }
+        */
